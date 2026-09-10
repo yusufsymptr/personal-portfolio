1d65ed685a7cf45287f11dabd4bc077dec3dd577
YusufSY03 <syamputra24ti@mahasiswa.pcr.ac.id>
2026-09-10 10:44:39 +0700

    Update Google verification token

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: "Yusuf Syam Putra | Software Engineer & UI/UX Designer",
   description: "Portofolio profesional Yusuf Syam Putra, mahasiswa Teknik Informatika di Politeknik Caltex Riau. Berpengalaman dalam pengembangan Full-stack (Next.js, Laravel), UI/UX (Figma), dan arsitektur data.",
   verification: {
-    google: 'google5e3774eeb27bc81f',
+    google: 'gPTwolPN2DIEg6IVrdELcHUHgGT5gaR1QHjQMi0iLW8U',
   },
   keywords: [
     "Yusuf Syam Putra", 
